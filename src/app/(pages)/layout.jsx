1d669c090d094ae795905db6e01254d348cb0cf1
shubham <shubhamshrm02@gmail.com>
2025-05-05 12:27:12 +0530

    remove auth

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,16 +1,18 @@
-import AuthProtectionWrapper from '@/components/wrappers/AuthProtectionWrapper';
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 const ScrollToTop = dynamic(() => import('@/components/ScrollToTop'));
+
 const HorizontalLayout = ({
   children
 }) => {
-  return <AuthProtectionWrapper>
+  return (
+    <>
       <Suspense fallback={<div />}>{children}</Suspense>
-
       <Suspense fallback={<div />}>
         <ScrollToTop />
       </Suspense>
-    </AuthProtectionWrapper>;
+    </>
+  );
 };
+
 export default HorizontalLayout;
