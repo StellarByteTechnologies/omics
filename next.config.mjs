6c86044f91edc643ed414cb5a25e9495c124b925
shubham <shubhamshrm02@gmail.com>
2025-05-09 18:55:59 +0530

    updated deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // 👈 This enables static export
+  output: 'export',
 };
 
 export default nextConfig;
